2b17fae0d2f676e8c82fa53a5936632840e79212
Alastair Paragas <alastairparagas@gmail.com>
2016-08-07 11:10:45 -0400

    Twitter API done

---
+++ api_twitter.py
@@ -0,0 +1,41 @@
+import os
+
+import twitter
+
+consumer_key = os.getenv('TWITTER_CONSUMER_KEY')
+consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET')
+access_token_key = os.getenv('TWITTER_TOKEN')
+access_token_secret = os.getenv('TWITTER_TOKEN_SECRET')
+
+def obtain(search_term, category):
+  
+  api = twitter.Api(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token_key=access_token_key,
+    access_token_secret=access_token_secret
+  )
+  
+  try:
+    tweets = api.GetSearch(
+      term="%s" % search_term
+    )
+  except BaseException:
+    return None
+  
+  print tweets[0].user
+
+  return {
+    "metadata": {
+      "type": "twitter-tweet",
+      "term": search_term,
+      "category": "category"
+    },
+    "data": {
+      "author": tweets[0].user.name,
+      "authorImage": tweets[0].user.profile_image_url,
+      "tweet": tweets[0].text
+    }
+  }
+  
+  
